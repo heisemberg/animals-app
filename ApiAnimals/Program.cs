28c0e818f37a521eeef6a6a667fc1b69bc2d502e
Heisemberg David Soler <heissen1122@gmail.com>
2023-10-05 13:08:34 -0500

    context

--- ApiAnimals/Program.cs
+++ ApiAnimals/Program.cs
@@ -1,3 +1,6 @@
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,7 +10,13 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<AnimalsContext>(options =>{
+    string connectionString = builder.Configuration.GetConnectionString("MySqlConex");
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+});
+
 var app = builder.Build();
+    
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
